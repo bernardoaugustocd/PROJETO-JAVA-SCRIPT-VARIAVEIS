4755e9d29b3fd98fe74c437f48cb9e481d540895
SITE_JURIDICO <bernardoaugustocd@gmail.com>
2026-01-16 20:42:01 -0300

    PROJETO-JAVA-SCRIPT-VARIAVEIS|BERNARDO: Criação do index e código do JAVASCRIPT para variáveis

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome_pessoa, genero_pessoa, idade_pessoa;
+
+nome_pessoa = "Bernardo";
+genero_pessoa = "Masculino";
+idade_pessoa = 15;
+
+console.log(" Olá mundo. Meu nome é " + nome_pessoa + " e tenho " + idade_pessoa + " anos de idade. " + "Meu gênero é " + genero_pessoa);
